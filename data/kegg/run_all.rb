df4adcd7fa00c725dba792fbc02f745b5b3456ad
Haitham Gabr <hgabr@cise.ufl.edu>
2014-05-22 14:56:43 -0400

    adjusting run_all.rb to linux

--- data/kegg/run_all.rb
+++ data/kegg/run_all.rb
@@ -1,7 +1,9 @@
 #This script runs preach_prob on all correlation files in a dataset directory
 dataset = ARGV[0]
 Dir.glob("#{dataset}/coexp_*.txt").each do |coexp_file|
-	puts "../../bin/Release/preach_prob.exe #{dataset}/#{dataset}_hsa.txt #{coexp_file} #{coexp_file.gsub("coexp", "prob").gsub(".txt", ".out")}"
-	res = `../../bin/Release/preach_prob.exe #{dataset}/#{dataset}_hsa.txt #{coexp_file} #{coexp_file.gsub("coexp", "prob").gsub(".txt", ".out")}`
+	#puts "../../bin/Release/preach_prob.exe #{dataset}/#{dataset}_hsa.txt #{coexp_file} #{coexp_file.gsub("coexp", "prob").gsub(".txt", ".out")}"
+	#res = `../../bin/Release/preach_prob.exe #{dataset}/#{dataset}_hsa.txt #{coexp_file} #{coexp_file.gsub("coexp", "prob").gsub(".txt", ".out")}`
+	puts "../../eparc #{dataset}/#{dataset}_hsa.txt #{coexp_file} #{coexp_file.gsub("coexp", "prob").gsub(".txt", ".out")}"
+	res = `../../eparc #{dataset}/#{dataset}_hsa.txt #{coexp_file} #{coexp_file.gsub("coexp", "prob").gsub(".txt", ".out")}`
 	puts res.split("\n").last
 end
