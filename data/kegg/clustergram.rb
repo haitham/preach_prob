6dfe17406a7b180fc3cacdc44eaf9b46bbadd585
Haitham Gabr <hgabr@cise.ufl.edu>
2014-05-30 10:33:31 -0400

    primary clustergarms for erbb and wnt

---
+++ data/kegg/clustergram.rb
@@ -0,0 +1,21 @@
+#This script reads all prob output files in subdir
+#And produces a clustergram matlab file
+subdir = ARGV[0]
+matrix = ""
+row_labels = ""
+Dir.glob("#{subdir}/prob_*.out").each do |filename|
+	row_labels = "#{row_labels} '#{filename.split("prob_").last.split(".out").first}'"
+	open filename do |f|
+		until (line = f.gets).nil?
+			next if line.strip.empty? or line =~ /(^Result)|(UNKNOWN)/
+			matrix = "#{matrix} #{line.strip.split.last}"
+		end
+	end
+	matrix = "#{matrix}\n"
+end
+
+open "#{subdir}/clusterprob.m", "w" do |fout|
+	fout.puts "RowLabels = {#{row_labels}}"
+	fout.puts "Matrix = [#{matrix}]"
+	fout.puts "CG = clustergram(Matrix, 'RowLabels', RowLabels, 'Symmetric', false, 'ColorMap', colormap('JET'))"
+end
